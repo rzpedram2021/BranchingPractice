f8fcec8d3711e8e95568d452680a136f4d1481e6
Reza Pedram <reza.pedram@tekschool.us>
2023-04-11 22:42:51 -0700

    this is another practice

---
+++ src/pullAndPushPracticeInGithub/branch1.java
@@ -0,0 +1,15 @@
+package pullAndPushPracticeInGithub;
+
+public class branch1 {
+
+	public static void main(String[] args) {
+		String i = "hello";
+		System.out.println(i);
+
+	}
+	
+	public static void ILikeGit() {
+		
+	}
+
+}
